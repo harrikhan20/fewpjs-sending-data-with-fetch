6c14bef704f87959a250ae7eccd9ef5ebe64ae2f
harris <khanharris7@yahoo.com>
2021-06-23 20:22:53 -0500

    commit tree 77ab39e7b8c0bed0f3af345cffeab708200ddc10

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+
+  
+  // method: "POST" is missing from the object below
+ 
+
+  const submitData = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+  
+  fetch("http://localhost:3000/users", configurationObject)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      console.log(object);
+    }); 
